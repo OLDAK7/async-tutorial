f5af754dd90f4f5ab923f5aaa63b2fc5a9b12862
OLDAK7 <almaz.karimovriddle@mail.ru>
2022-06-08 15:23:08 +0500

    change index

--- index.js
+++ index.js
@@ -16,74 +16,73 @@ const __filename = fileURLToPath(import.meta.url);  // index.js
 const __dirname = path.dirname(__filename);  // async 
 
 
+console.log(chalk.bgBlue(__filename)); // D:\торрент\www\test-electron\async\index.js
+console.log(chalk.bgGreen(__dirname)); // D:\торрент\www\test-electron\async
 
-// console.log(chalk.bgBlue(__filename)); // D:\торрент\www\test-electron\async\index.js
-// console.log(chalk.bgGreen(__dirname)); // D:\торрент\www\test-electron\async
-
-// console.log("Название файла: ", path.basename(__filename)); // index.js
-// console.log("Имя директории: ", path.basename(__dirname)); // async
-// console.log(chalk.bgGreenBright("Расширение файла: ", path.extname(__filename)));
-// console.log('Parse: ', path.parse(__filename));
+console.log("Название файла: ", path.basename(__filename)); // index.js
+console.log("Имя директории: ", path.basename(__dirname)); // async
+console.log(chalk.bgGreenBright("Расширение файла: ", path.extname(__filename)));
+console.log('Parse: ', path.parse(__filename));
 
 /* создание папки(только уникальные названия, если повторяется, то выдает ошибку) */
 
-// fs.mkdir(path.join(__dirname, 'jsFiles'),(err) => {
-//     if (err) {
-//         throw err;
-//     }
-//     console.log('Папка создана');
-// })
+fs.mkdir(path.join(__dirname, 'jsFiles'),(err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Папка создана');
+})
 
 
-// const filePath = path.join(__dirname, 'jsFiles', 'checker.js');
-// const data = `let str = "some Data"`;
+const filePath = path.join(__dirname, 'jsFiles', 'checker.js');
+const data = `let str = "some Data"`;
 // const filePath = path.join(__dirname, 'files', 'text2.txt');
-// write(filePath, data);
-// print('./files/text.txt');
+write(filePath, data);
+print('./files/text.txt');
 
 
-// compareFileSizes(__filename, './files/text.txt', (_err, result) => console.log(result))
+compareFileSizes(__filename, './files/text.txt', (_err, result) => console.log(result))
 
-// const firstPath = path.join(__dirname, 'files', 'text2.txt');
-// const secondPath = path.join(__dirname, 'files','text.txt');
+const firstPath = path.join(__dirname, 'files', 'text2.txt');
+const secondPath = path.join(__dirname, 'files','text.txt');
 
-// move(firstPath, secondPath, (error) => {
-//     if (error) {
-//         console.log('oops');
-//         return;
-//     }
-//     console.log(chalk.bgBlue('yes!'));
-// });
+move(firstPath, secondPath, (error) => {
+    if (error) {
+        console.log('oops');
+        return;
+    }
+    console.log(chalk.bgBlue('yes!'));
+});
 
-// getDirectorySize(__dirname, (err, size) => {
-//     console.log(size);
-// })
+getDirectorySize(__dirname, (err, size) => {
+    console.log(size);
+})
 
-// const id = watch('./files/text.txt', 500, (err) => {
-//     if(err) throw err;
-//     console.log(chalk.green('Wow!'));
-// });
+const id = watch('./files/text.txt', 500, (err) => {
+    if(err) throw err;
+    console.log(chalk.green('Wow!'));
+});
 
-// setTimeout(() => fs.appendFileSync('./files/text.txt', '\n ehu'), 700);
-// setTimeout(() => clearInterval(id), 5000);
+setTimeout(() => fs.appendFileSync('./files/text.txt', '\n ehu'), 700);
+setTimeout(() => clearInterval(id), 5000);
 
-// unionFiles('./files/text.txt', './files/text2.txt', './files/newText.txt', (err)=>{
-//     if(err) throw err;
-// })
+unionFiles('./files/text.txt', './files/text2.txt', './files/newText.txt', (err)=>{
+    if(err) throw err;
+})
 
-// reverse('./files/newText.txt');
+reverse('./files/newText.txt');
 
-// touch('./files/text.txt');
+touch('./files/text.txt');
 
 
-// const filePaths = ['./files', './files/tex.txt', './files/newText.txt'];
-// getTypes(filePaths);
+const filePaths = ['./files', './files/tex.txt', './files/newText.txt'];
+getTypes(filePaths);
 
-// getDirSize(__dirname);
+getDirSize(__dirname);
 
-// wait(2500).then(() => console.log('time is over!'))
+wait(2500).then(() => console.log('time is over!'))
 
-// exchange('./files/text.txt','./files/text2.txt');
+exchange('./files/text.txt','./files/text2.txt');
 
 const url = 'http://almaz.com';
 const links = getBadLinks(url)
